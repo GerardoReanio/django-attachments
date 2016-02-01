71328d3b1f54f2448acf1c2136bc8b2105f52079
GerardoReanio <gerardo.reanio@gmail.com>
2016-02-01 17:55:24 -0500

    Update views.py

--- attachments/views.py
+++ attachments/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.http import HttpResponseRedirect
-from django.db.models.loading import get_model
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext, ugettext_lazy as _
 from django.template.context import RequestContext
@@ -9,6 +8,13 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from attachments.models import Attachment
 from attachments.forms import AttachmentForm
+
+
+try:
+    from django.apps import apps
+    get_model = apps.get_model
+except:
+    from django.db.models.loading import get_model
 
 def add_url_for_obj(obj):
     return reverse('add_attachment', kwargs={
